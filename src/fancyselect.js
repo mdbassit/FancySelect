f9f1129f1267c1a782e84dfbaa0aebbc2c2cdee5
Momo Bassit <momo@mbassit.com>
2021-10-19 15:59:29 +0800

    Select an item when it's clicked

--- src/fancyselect.js
+++ src/fancyselect.js
@@ -1,8 +1,18 @@
 document.querySelectorAll('.fsb-select button').forEach(button => {
+  const list = button.nextElementSibling;
+
   button.addEventListener('click', event => {
     event.target.setAttribute('aria-expanded', 'true');
     event.stopPropagation();
   });
+
+  for (const item of list.children) {
+    item.addEventListener('click', event => {
+      list.querySelector('[aria-selected="true"]').setAttribute('aria-selected', 'false');
+      item.setAttribute('aria-selected', 'true');
+      button.innerHTML = item.innerHTML;
+    });
+  }
 });
 
 document.addEventListener('click', event => {
