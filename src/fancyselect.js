f0b8e7acbd664b47d84b867596568644d19ae279
Momo Bassit <momo@mbassit.com>
2021-10-19 16:50:12 +0800

    Wrap the code in a self-invoking function

--- src/fancyselect.js
+++ src/fancyselect.js
@@ -1,28 +1,31 @@
-document.querySelectorAll('.fsb-select button').forEach(button => {
-  const list = button.nextElementSibling;
-  const widthAdjuster = list.nextElementSibling;
-
-  // Used to force the select box to take the width of the longest item by default
-  widthAdjuster.innerHTML = `<div style="width: ${list.firstElementChild.offsetWidth}px;"></div>`;
-
-  button.addEventListener('click', event => {
-    event.target.setAttribute('aria-expanded', 'true');
-    event.stopPropagation();
-  });
-
-  for (const item of list.children) {
-    button.innerHTML = list.querySelector('[aria-selected="true"]').innerHTML;
-
-    item.addEventListener('click', event => {
-      list.querySelector('[aria-selected="true"]').setAttribute('aria-selected', 'false');
-      item.setAttribute('aria-selected', 'true');
-      button.innerHTML = item.innerHTML;
-    });
-  }
-});
-
-document.addEventListener('click', event => {
+(function (document) {
   document.querySelectorAll('.fsb-select button').forEach(button => {
-    button.setAttribute('aria-expanded', 'false');
+    const list = button.nextElementSibling;
+    const widthAdjuster = list.nextElementSibling;
+
+    // Used to force the select box to take the width of the longest item by default
+    widthAdjuster.innerHTML = `<div style="width: ${list.firstElementChild.offsetWidth}px;"></div>`;
+
+    button.addEventListener('click', event => {
+      event.target.setAttribute('aria-expanded', 'true');
+      event.stopPropagation();
+    });
+
+    for (const item of list.children) {
+      button.innerHTML = list.querySelector('[aria-selected="true"]').innerHTML;
+
+      item.addEventListener('click', event => {
+        list.querySelector('[aria-selected="true"]').setAttribute('aria-selected', 'false');
+        item.setAttribute('aria-selected', 'true');
+        button.innerHTML = item.innerHTML;
+      });
+    }
   });
-});
+
+  document.addEventListener('click', event => {
+    document.querySelectorAll('.fsb-select button').forEach(button => {
+      button.setAttribute('aria-expanded', 'false');
+    });
+  });
+})(document)
+  
